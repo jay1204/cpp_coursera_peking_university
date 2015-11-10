6422c500146333a567cd9a6c43fba8de6bbff1c0
jay1204 <zchen22@ncsu.edu>
2015-11-09 21:03:52 -0500

    addition

---
+++ Assignment_week2/source.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student{
+private:
+    string name;
+    int age;
+    string student_id;
+    int grade[4];
+public:
+    void input(){
+        getline(cin,name,',');
+        cin >> age ;
+        cin.ignore();
+        getline(cin,student_id,',');
+        for(int i=0;i<4;i++){
+            cin >> grade[i];
+            cin.ignore();
+        }
+    }
+    
+    void output(){
+        cout << name << "," << age << "," << student_id << ",";
+        cout << getAverageGrade() << endl;
+    }
+    
+    int getAverageGrade(){
+        int sum=0;
+        int len = sizeof(grade)/sizeof(grade[0]);
+        for(int i=0;i<len;i++){
+            sum+=grade[i];
+        }
+        return sum/len;
+    }
+};
